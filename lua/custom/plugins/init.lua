27df55948528d8610454b577739bc5f44acd0fe8
Clement BOUVET <bouvetcl@gmail.com>
2024-09-16 09:51:29 +0200

    custom: enable persisted.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'olimorris/persisted.nvim',
+    config = function()
+      require('persisted').setup {
+        use_git_branch = true, -- create session files based on the branch of the git enabled repository
+        autosave = true, -- automatically save session files when exiting Neovim
+        autoload = true, -- automatically load the session for the cwd on Neovim startup
+        on_autoload_no_session = function()
+          vim.notify 'No existing session to load.'
+        end,
+      }
+    end,
+  },
+}
