da37036d4ed7097d733a32059f065d04fbf8786e
Clement BOUVET <bouvetcl@gmail.com>
2024-09-16 11:49:30 +0200

    enable mini.files

--- init.lua
+++ init.lua
@@ -866,6 +866,7 @@ require('lazy').setup({
         return '%2l:%-2v'
       end
 
+      require("mini.files").setup()
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
---
+++ lua/custom/keymaps.lua
@@ -0,0 +1,1 @@
+vim.keymap.set('n', '-', '<CMD>lua MiniFiles.open();<CR>', { desc = 'Open MiniFiles' })
