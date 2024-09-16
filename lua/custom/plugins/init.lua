d2a420732de56899495902234e2b5bc91debff35
Clement BOUVET <bouvetcl@gmail.com>
2024-09-16 11:45:13 +0200

    add persisted.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,16 +3,29 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'akinsho/toggleterm.nvim',
-    version = '*',
-    opts = {
-      open_mapping = [[<C-\>]],
-      persist_mode = false, -- does not play nice with auto insert mode autocmds
-      direction = 'float',
-      float_opts = {
-        border = 'curved',
-      },
+    {
+        'akinsho/toggleterm.nvim',
+        version = '*',
+        opts = {
+            open_mapping = [[<C-\>]],
+            persist_mode = false, -- does not play nice with auto insert mode autocmds
+            direction = 'float',
+            float_opts = {
+                border = 'curved',
+            },
+        },
     },
-  },
+    {
+        'olimorris/persisted.nvim',
+        config = function()
+            require('persisted').setup {
+                use_git_branch = true, -- create session files based on the branch of the git enabled repository
+                autosave = true,       -- automatically save session files when exiting Neovim
+                autoload = true,       -- automatically load the session for the cwd on Neovim startup
+                on_autoload_no_session = function()
+                    vim.notify 'No existing session to load.'
+                end,
+            }
+        end,
+    },
 }
