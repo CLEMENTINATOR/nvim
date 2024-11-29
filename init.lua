b53f65aa1ed0883136c1f1739e08ca6b8e3a4d2d
Clement Bouvet <clement@cbouvet.fr>
2024-11-29 15:11:56 +0100

    lspconfig: add gopls

--- init.lua
+++ init.lua
@@ -650,6 +650,7 @@ require('lazy').setup({
       require('lspconfig').clangd.setup {}
       require('lspconfig').rust_analyzer.setup {}
       require('lspconfig').lua_ls.setup {}
+      require('lspconfig').gopls.setup {}
     end,
   },
 
