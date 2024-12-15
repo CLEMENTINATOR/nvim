3aae853d5fc6f74931efccefe35276f7cedcca71
Clement Bouvet <clement@cbouvet.fr>
2024-12-15 14:49:15 +0100

    test another copilot plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -29,25 +29,21 @@ return {
         end,
     },
     {
-    'zbirenbaum/copilot.lua',
-    opts = {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-          accept = '<m-y>',
-          accept_word = '<m-w>',
-          accept_line = '<m-l>',
-          next = '<m-]>',
-          prev = '<m-[>',
-          dismiss = '<m-d>',
-        },
-      },
-    },
+      'github/copilot.vim',
+      lazy = false,
+      init = function()
+          vim.keymap.set('i', '<M-w>', '<Plug>(copilot-accept-word)', { desc = 'Accept copilot word' })
+          vim.keymap.set('i', '<M-l>', '<Plug>(copilot-accept-line)', { desc = 'Accept copilot line' })
+          vim.keymap.set('n', '<leader>uc', function()
+              if vim.b.copilot_enabled == nil or vim.b.copilot_enabled then
+                  vim.b.copilot_enabled = false
+              else
+                  vim.b.copilot_enabled = true
+              end
+          end, { desc = 'Toggle Copilot' })
+      end,
   },
   { 'HiPhish/rainbow-delimiters.nvim', dependencies = { 'nvim-treesitter/nvim-treesitter' } },
-
   {
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
