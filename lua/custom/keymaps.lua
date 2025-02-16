1499debe437a18e6992619062741b2e690a2359e
Clement Bouvet <clement@cbouvet.fr>
2025-02-16 10:24:22 +0100

    add buffer delete keymap

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,1 +1,3 @@
+vim.keymap.set('n', '<leader>bO', '<cmd>%bd!|e#|bd!#<cr>', { desc = 'Delete all buffers except current' })
+
 vim.keymap.set('n', '-', '<CMD>lua MiniFiles.open();<CR>', { desc = 'Open MiniFiles' })
