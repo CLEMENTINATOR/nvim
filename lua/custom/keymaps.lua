d086ad92b6d2d1a1ee1285be117f963c5ee5be9b
Clement Bouvet <clement@cbouvet.fr>
2025-02-16 10:33:15 +0100

    mini.files: - open current path

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,3 +1,3 @@
 vim.keymap.set('n', '<leader>bO', '<cmd>%bd!|e#|bd!#<cr>', { desc = 'Delete all buffers except current' })
 
-vim.keymap.set('n', '-', '<CMD>lua MiniFiles.open();<CR>', { desc = 'Open MiniFiles' })
+vim.keymap.set('n', '-', '<CMD>lua MiniFiles.open(vim.api.nvim_buf_get_name(0)); MiniFiles.reveal_cwd()<CR>', { desc = 'Open MiniFiles' })
