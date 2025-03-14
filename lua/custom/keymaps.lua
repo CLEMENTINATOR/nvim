677335e231dc1b71fb110b708235e90b8dafe905
Clement Bouvet <clement@cbouvet.fr>
2025-03-14 09:09:49 +0100

    test snacks

--- lua/custom/keymaps.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', '<leader>bO', '<cmd>%bd!|e#|bd!#<cr>', { desc = 'Delete all buffers except current' })
-
-vim.keymap.set('n', '-', '<CMD>lua MiniFiles.open(vim.api.nvim_buf_get_name(0)); MiniFiles.reveal_cwd()<CR>', { desc = 'Open MiniFiles' })
