3b37ab2a46419ead72a34ebc13428c5c8a4438e8
Clement Bouvet <clement@cbouvet.fr>
2025-04-01 11:29:07 +0200

    enable virtual diagnostics

--- lua/custom/opts.lua
+++ lua/custom/opts.lua
@@ -3,3 +3,27 @@ vim.o.foldmethod = 'expr'
 vim.o.foldlevelstart = 99
 vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.opt.listchars = { tab = '╎ ', trail = '·', nbsp = '␣' }
+
+vim.diagnostic.config {
+  severity_sort = true,
+  float = { border = 'rounded', source = 'if_many' },
+  underline = true,
+  signs = vim.g.have_nerd_font and {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅚 ',
+      [vim.diagnostic.severity.WARN] = '󰀪 ',
+      [vim.diagnostic.severity.INFO] = '󰋽 ',
+      [vim.diagnostic.severity.HINT] = '󰌶 ',
+    },
+  } or {},
+  virtual_text = {
+    severity = {
+      max = vim.diagnostic.severity.WARN,
+    },
+  },
+  virtual_lines = {
+    severity = {
+      min = vim.diagnostic.severity.ERROR,
+    },
+  },
+}
