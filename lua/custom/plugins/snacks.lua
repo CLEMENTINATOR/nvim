53791d4cd8ae7040a2c84e154097b69c80d50bf8
Clement Bouvet <clement@cbouvet.fr>
2025-04-19 08:36:01 +0200

    snacks: formatting

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -106,7 +106,7 @@ return {
       {
         '<leader>fc',
         function()
-          Snacks.picker.files({ cwd = vim.fn.stdpath('config') })
+          Snacks.picker.files { cwd = vim.fn.stdpath 'config' }
         end,
         desc = 'Find Config File',
       },
@@ -127,7 +127,7 @@ return {
       {
         '<leader>fa',
         function()
-          Snacks.picker.files({ignored=true, hidden=true})
+          Snacks.picker.files { ignored = true, hidden = true }
         end,
         desc = 'Find All Files',
       },
@@ -538,7 +538,7 @@ return {
         '<leader>N',
         desc = 'Neovim News',
         function()
-          Snacks.win({
+          Snacks.win {
             file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
             width = 0.6,
             height = 0.6,
@@ -549,7 +549,7 @@ return {
               statuscolumn = ' ',
               conceallevel = 3,
             },
-          })
+          }
         end,
       },
     },
@@ -567,17 +567,17 @@ return {
           vim.print = _G.dd -- Override print to use snacks for `:=` command
 
           -- Create some toggle mappings
-          Snacks.toggle.option('spell', { name = 'Spelling' }):map('<leader>us')
-          Snacks.toggle.option('wrap', { name = 'Wrap' }):map('<leader>uw')
-          Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map('<leader>uL')
-          Snacks.toggle.diagnostics():map('<leader>ud')
-          Snacks.toggle.line_number():map('<leader>ul')
-          Snacks.toggle.option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map('<leader>uc')
-          Snacks.toggle.treesitter():map('<leader>uT')
-          Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map('<leader>ub')
-          Snacks.toggle.inlay_hints():map('<leader>uh')
-          Snacks.toggle.indent():map('<leader>ug')
-          Snacks.toggle.dim():map('<leader>uD')
+          Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
+          Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
+          Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map '<leader>uL'
+          Snacks.toggle.diagnostics():map '<leader>ud'
+          Snacks.toggle.line_number():map '<leader>ul'
+          Snacks.toggle.option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map '<leader>uc'
+          Snacks.toggle.treesitter():map '<leader>uT'
+          Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
+          Snacks.toggle.inlay_hints():map '<leader>uh'
+          Snacks.toggle.indent():map '<leader>ug'
+          Snacks.toggle.dim():map '<leader>uD'
         end,
       })
     end,
