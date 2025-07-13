bb9f2e312451f8ae51f009c39facb31d7bc14f5a
Clement Bouvet <clement@cbouvet.fr>
2025-07-13 09:35:02 +0200

    fix markview/treesitter order

--- init.lua
+++ init.lua
@@ -552,6 +552,7 @@ require('lazy').setup({
         },
       },
     },
+    dependencies = { 'OXY2DEV/markview.nvim' },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
