563c47f747925a06bfa8667453391be3a9c7a0e7
Clement Bouvet <clement@cbouvet.fr>
2025-07-13 10:11:26 +0200

    switch to gruv

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -3,14 +3,14 @@ return {
     'ellisonleao/gruvbox.nvim',
     opts = { transparent_mode = true },
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      vim.cmd.colorscheme 'gruvbox'
+    end,
   },
   {
     'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
     opts = {},
-    init = function()
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
   },
 }
