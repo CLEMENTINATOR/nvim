6d37ad0517dc00084604db3776035e50e359e496
Clement Bouvet <clement@cbouvet.fr>
2025-10-10 16:36:13 +0200

    yoink sidekick.nvim

---
+++ lua/custom/plugins/ai.lua
@@ -0,0 +1,41 @@
+local toggle_copilot = function()
+  if vim.b.copilot_enabled == nil or vim.b.copilot_enabled then
+    vim.b.copilot_enabled = false
+    vim.print 'Copilot disabled'
+  else
+    vim.b.copilot_enabled = true
+    vim.print 'Copilot enabled'
+  end
+end
+
+return {
+  {
+    'github/copilot.vim',
+    lazy = false,
+    init = function()
+      vim.keymap.set('i', '<M-w>', '<Plug>(copilot-accept-word)', { desc = 'Accept copilot word' })
+      vim.keymap.set('i', '<M-l>', '<Plug>(copilot-accept-line)', { desc = 'Accept copilot line' })
+      vim.keymap.set('n', '<leader>uc', toggle_copilot, { desc = 'Toggle Copilot' })
+      vim.keymap.set('i', '<M-u>', toggle_copilot, { desc = 'Toggle Copilot' })
+    end,
+  },
+  {
+    'folke/sidekick.nvim',
+    lazy = false,
+    opts = {
+      nes = {
+        enabled = true,
+        debounce = 100,
+      },
+    },
+    keys = {
+      {
+        '<leader>cc',
+        function()
+          require('sidekick.cli').toggle { name = 'claude' }
+        end,
+        desc = 'Toggle Sidekick CLI',
+      },
+    },
+  },
+}
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -16,21 +16,6 @@ return {
       }
     end,
   },
-  {
-    'github/copilot.vim',
-    lazy = false,
-    init = function()
-      vim.keymap.set('i', '<M-w>', '<Plug>(copilot-accept-word)', { desc = 'Accept copilot word' })
-      vim.keymap.set('i', '<M-l>', '<Plug>(copilot-accept-line)', { desc = 'Accept copilot line' })
-      vim.keymap.set('n', '<leader>uc', function()
-        if vim.b.copilot_enabled == nil or vim.b.copilot_enabled then
-          vim.b.copilot_enabled = false
-        else
-          vim.b.copilot_enabled = true
-        end
-      end, { desc = 'Toggle Copilot' })
-    end,
-  },
   { 'nvim-treesitter/nvim-treesitter-context', dependencies = { 'nvim-treesitter/nvim-treesitter' }, opts = { max_lines = 8 } },
   {
     'OXY2DEV/markview.nvim',
